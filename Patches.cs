08ad9074301796d76fc1c455670cd782b535c49e
SydMontague <sydmontague@phoenix-staffel.de>
2024-02-14 14:29:13 +0100

    initial commit

---
+++ Patches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+
+namespace DWNO_UnlockAllRequirements
+{
+  class Patches
+  {
+    [HarmonyPatch(typeof(PlayerData), "GetEvolutionConditionFlag") ]
+    [HarmonyPatch(new Type[] { typeof(uint), typeof(uint) })]
+    [HarmonyPrefix]
+    static bool GetEvolutionConditionFlag_Prefix(uGenelogyInformationUI __instance, ref bool __result, uint id, uint flagIndex)
+    {
+      __result = true;
+      return false;
+    }
+
+  }
+}
---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System;
+
+namespace DWNO_UnlockAllRequirements;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    public static Plugin Instance;
+  public override void Load()
+  {
+    Instance = this;
+
+    // Plugin startup logic
+    Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    Harmony.CreateAndPatchAll(typeof(Patches));
+  }
+}
